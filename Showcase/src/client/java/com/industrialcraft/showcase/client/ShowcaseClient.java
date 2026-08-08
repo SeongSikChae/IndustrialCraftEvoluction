3b926e4ffdc1f617858359b8aef2f0eea2a4b799
ChaeSeongSik <css5831@outlook.com>
2026-08-08 13:12:49 +0900

    fix: Showcase 관련 일체 제거, 필요시 별도 프로젝트로 재생성 할 예정.

--- Showcase/src/client/java/com/industrialcraft/showcase/client/ShowcaseClient.java
+++
@@ -1,9 +0,0 @@
-package com.industrialcraft.showcase.client;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class ShowcaseClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-	}
-}
